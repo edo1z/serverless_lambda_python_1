087b713c7659b2a792763c219f963857fc365f00
web3ten0 <web3ten0@gmail.com>
2022-04-10 20:26:00 +0900

    added docker-compose(serverless-offline)

--- src/hello.py
+++ src/hello.py
@@ -2,8 +2,7 @@ import json
 
 def hello(event, context):
     body = {
-        "message": "Go Serverless v3.0! Your function executed successfully!",
-        "input": event,
+        "message": "hello world",
     }
     response = {"statusCode": 200, "body": json.dumps(body)}
     return response
